2e7793e5cd78d2548964c672ced0b42cb946a62e
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-05-15 13:40:29 +0530

    Update 5.data_types.js

--- 2_js_intro_basic_concept/5.data_types.js
+++ 2_js_intro_basic_concept/5.data_types.js
@@ -1,57 +1,95 @@
-// data type in js
+// // data type in js
 
-// total 8 type of variable in js
+// // total 8 type of variable in js
 
-// 1. string
+// // 1. string
 
-// 2.Number
+// // 2.Number
 
-// 3.Boolean
+// // 3.Boolean
 
-// 4.undefined datatype
+// // 4.undefined datatype
 
-// 5.bigint
+// // 5.bigint
 
-// 6.null
+// // 6.null
 
-// 7.Boolean
+// // 7.Boolean
 
-// 8.symbol
+// // 8.symbol
 
-// 1.string datatype
+// // 1.string datatype
 
-let name = "sarvaiya";
+// let name = "sarvaiya";
 
-console.log("my name is", name);
+// console.log("my name is", name);
 
-console.log("type of variable", typeof name);
+// console.log("type of variable", typeof name);
 
-// 2.number datatype
+// // 2.number datatype
 
-let num = 6355907071;
+// let num = 6355907071;
 
-console.log("number is", num);
+// console.log("number is", num);
 
-console.log("type of variable", typeof num);
+// console.log("type of variable", typeof num);
 
-// 3.Boolean datatype
+// // 3.Boolean datatype
 
-let bool = true;
+// let bool = true;
 
-console.log("boolean", bool);
+// console.log("boolean", bool);
 
-console.log("type of variable", typeof bool);
+// console.log("type of variable", typeof bool);
 
-let bool2 = false;
+// let bool2 = false;
 
-console.log("boolean", bool2);
+// console.log("boolean", bool2);
 
-console.log("type of variable", typeof bool);
+// console.log("type of variable", typeof bool);
 
-// 4. Undefined Datatype
+// // 4. Undefined Datatype
 
-let un;
+// let un;
 
-console.log("Undefined ", un);
+// console.log("Undefined ", un);
 
-console.log("type of value :", typeof un);
+// console.log("type of value :", typeof un);
+
+
+
+// 5. null Datatype
+
+// let n = null;
+
+// console.log("null",n);
+// console.log("type of value :", typeof n);
+
+
+
+// 6. object Datatype
+
+// let obj = {name :   "parthrajsinh", name :"gyandipsinh"};
+
+// console.log("object",obj);
+// console.log("type of value :",typeof obj);
+
+
+// 7. symbol Datatype
+
+// let sym = Symbol("id");
+
+// console.log("symbol",sym);
+// console.log("type of value :", typeof sym);
+
+
+// 8. Bigint Datatype
+
+
+// let big = 63559070717990029329n;
+
+// console.log("Bigint",big);
+// console.log("type of value :", typeof big);
+
+
+
