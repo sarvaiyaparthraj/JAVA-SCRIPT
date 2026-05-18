d6bc178f72c7f6f1ec41b12110d2dfa51a35abe6
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-05-18 14:26:04 +0530

    Update 8.operator.js

--- 2_js_intro_basic_concept/8.operator.js
+++ 2_js_intro_basic_concept/8.operator.js
@@ -8,17 +8,146 @@
 
 
 
-let a = 19;
-let b = 29;
+// let a = 19;
+// let b = 29;
 
-console.log("Addition :",a+b);
+// console.log("Addition :",a+b);
 
-console.log("Subtraction :",a-b);
+// console.log("Subtraction :",a-b);
 
-console.log("Multiplication :",a*b);
+// console.log("Multiplication :",a*b);
 
-console.log("Division :",a/b);
+// console.log("Division :",a/b);
 
-console.log("Modulo :",a%b);
+// console.log("Modulo :",a%b);
 
-console.log("Exponentiation  :",a**b);
+// console.log("Exponentiation  :",a**b);
+
+
+
+
+
+
+// // 2.Assignment operator
+
+
+// a =  15;
+// b =+ 15;
+
+// console.log("add and assign :", a );
+// a -= 20;
+
+// console.log("Subtract and assign :",a);
+
+
+// a *= 5;
+
+// console.log("Multiple and assign :",a);
+
+
+// a /= 15;
+
+// console.log("Divide and assign :",a);
+
+
+
+// 3. Comparison operator
+
+
+// console.log("Equal :", 20 == 20);
+
+// console.log("Strict Equal     :",40 === 40);
+// console.log("grate than       :",20 > 30);
+// console.log("less than        :",100<200);
+// console.log("grate than equal :",20 >= 30);
+// console.log("less than equal  :",100<=200);
+// console.log("less than equal  :",100!=200);
+
+
+
+// // 4. Logical operator
+
+// let num1 = true;
+
+// let num2 = false;
+
+// console.log("AND :",num1 && num2); 
+
+// console.log("OR  :",num1 || num2);
+
+// console.log("NOT :", !num2);
+
+
+// // 5. Unary operator
+
+
+
+//  a = 50;
+
+// console.log("current value :",a);
+
+// console.log("Increment :",++a);
+
+// console.log("Pre-increment :",a++);
+
+// console.log("Decrement :",--a);
+
+// console.log("Pre-Decrement :",a--);
+
+// console.log("current value :",a);
+
+// // 6. Typeof operator
+
+
+// console.log("type of :",typeof 12);
+
+// console.log("type of :",typeof "n")
+
+
+
+
+
+
+// // Arithmetic operator
+
+// console.log("addition",10+10);
+// console.log("Subtraction",10-10);
+// console.log("Multiple",10*10);
+// console.log("division",10/10);
+// console.log("modula",10%10);
+// console.log("Exponentiation",10**10);
+
+
+// // Assignment operator
+
+// a = 10;
+
+// a += 12;
+
+
+// console.log("add and assign", a);
+
+// a *= 12;
+
+// console.log("add and multiply", a);
+
+
+
+
+// // comparison operator
+
+// console.log("equal ",   10=="10");
+// console.log("strict equal ", 10==="10");
+// console.log("grater than ",100 > 200);
+// console.log("less than", 100 < 200);
+// console.log("not equal", 100 != 100);
+
+// // logical operator
+
+//  num1 = true;
+//  num2 = false;
+
+
+// console.log("and", num1 && num2) ;
+// console.log("or",num1  || num2 );
+// console.log("not ", !num2);
