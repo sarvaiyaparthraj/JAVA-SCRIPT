83de0d2c4e0042134d187f90578ae39392bb0272
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-05-20 13:36:56 +0530

    Add files via upload

---
+++ 4.fuction/1.function.js
@@ -0,0 +1,25 @@
+
+
+
+function jsFunction(){
+    console.log("js function created");
+
+
+}
+
+
+jsFunction();
+
+
+function hello() {
+    console.log("Hello JavaScript");
+}
+
+hello();
+
+
+function welcome() {
+    console.log("Welcome User");
+}
+
+welcome();
---
+++ 4.fuction/2.function(parameter and arguments).js
@@ -0,0 +1,21 @@
+function play(game) {
+  console.log("The is game name " + game);
+}
+
+
+play("FREEFIRE");
+
+
+
+function student(name) {
+    console.log("Student Name is " + name);
+}
+
+student("Parth");
+
+
+function city(cityName) {
+    console.log("City Name is " + cityName);
+}
+
+city("Bhavnagar");
