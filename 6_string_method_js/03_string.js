678a0d7b0e9ce00a7214ee7d0417e88c4e6a4c69
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-05-28 13:39:45 +0530

    Add files via upload

---
+++ 6_string_method_js/03_string.js
@@ -0,0 +1,159 @@
+let text1 = "my name is sarvaiya parthrajsinh";
+
+console.log(" split string into array without space :",text1.split(""));
+console.log(" split string into array without added :",text1.split(""));
+console.log(" will remove to specific value in a text and return string :",text1.split("my"));
+ 
+
+
+let text2 = "game play out dor";
+
+console.log(" split string into array without space :",text2.split(""));
+console.log(" split string into array without added :",text2.split(""));
+console.log(" will remove to specific value in a text and return string :",text2.split("play"));
+
+
+
+console.log("text",text1);
+console.log("substring example", text1.substring(2));
+console.log("substring : ",text1.substring(1,4));
+console.log("substring : ",text1.substring(4,1));
+
+// toLocalUpperCase 
+
+let text3 = "how are you";
+console.log("uppercase : ",text3.toLocaleUpperCase());
+
+// ToLocaleLowercase
+
+let text4 = "HOE ARE YOU";
+console.log("lowercase : ",text4.toLocaleLowerCase());
+
+// trim Return a string with remove whitespace
+
+let text5 = "                  parthrajsinh                     ";
+console.log("count a length : ",text5.length);
+
+console.log("trim :",text5.trim());
+
+console.log("TrimStart : ",text5.trimStart());
+
+console.log("TrimStart : ",text5.trimEnd());
+
+
+
+
+
+
+
+// Example 2
+
+let text1 = "my village is bhavnagar";
+
+console.log(
+  "split string into array without space :",
+  text1.split("")
+);
+
+console.log(
+  "split string into array with space :",
+  text1.split(" ")
+);
+
+console.log(
+  "remove specific word and return array :",
+  text1.split("village")
+);
+
+console.log("text :", text1);
+
+console.log("substring example :", text1.substring(3));
+
+console.log("substring :", text1.substring(1, 6));
+
+console.log("substring :", text1.substring(6, 1));
+
+
+// toLocaleUpperCase
+
+let text2 = "javascript is easy";
+
+console.log("uppercase :", text2.toLocaleUpperCase());
+
+
+// toLocaleLowerCase
+
+let text3 = "HELLO STUDENT";
+
+console.log("lowercase :", text3.toLocaleLowerCase());
+
+
+// trim
+
+let text4 = "            sarvaiya parthrajsinh           ";
+
+console.log("count length :", text4.length);
+
+console.log("trim :", text4.trim());
+
+console.log("trimStart :", text4.trimStart());
+
+console.log("trimEnd :", text4.trimEnd());
+
+
+
+
+// Example 3
+
+
+let course = "i am learning javascript";
+
+console.log(
+  "split string into array without space :",
+  course.split("")
+);
+
+console.log(
+  "split string into array with space :",
+  course.split(" ")
+);
+
+console.log(
+  "remove specific word and return array :",
+  course.split("learning")
+);
+
+console.log("text :", course);
+
+console.log("substring example :", course.substring(5));
+
+console.log("substring :", course.substring(2, 9));
+
+console.log("substring :", course.substring(9, 2));
+
+
+// toLocaleUpperCase
+
+let text5 = "welcome to india";
+
+console.log("uppercase :", text5.toLocaleUpperCase());
+
+
+// toLocaleLowerCase
+
+let text6 = "GOOD MORNING FRIEND'S";
+
+console.log("lowercase :", text6.toLocaleLowerCase());
+
+
+// trim
+
+let text7 = "             gujarat india             ";
+
+console.log("count length :", text7.length);
+
+console.log("trim :", text7.trim());
+
+console.log("trimStart :", text7.trimStart());
+
+console.log("trimEnd :", text7.trimEnd());
