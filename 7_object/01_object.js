99f544861736511293e32c48dd52bf1fdc6dcf72
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-05-28 13:40:10 +0530

    Add files via upload

---
+++ 7_object/01_object.js
@@ -0,0 +1,72 @@
+const con ={
+    name: "parthrajinh",
+    age: 19,
+    address: "bvn"
+}
+
+console.log (con);
+
+console.log("type of",typeof con)
+
+const ob = new Object();
+
+console.log(typeof ob);
+
+ob.name="gyandipsinh";
+ob.age=19;
+ob.address="bnv";
+
+
+console.log("object detail :",ob);
+
+
+
+
+
+// Example 2
+
+
+const student = {
+  name: "Parthrajsinh",
+  age: 19,
+  city: "Bhavnagar"
+};
+
+console.log(student);
+
+console.log("type of :", typeof student);
+
+const obj1 = new Object();
+
+console.log(typeof obj1);
+
+obj1.name = "Sarvaiya";
+obj1.age = 20;
+obj1.city = "Rajkot";
+
+console.log("object detail :", obj1);
+
+
+
+// Example 3
+
+
+const employee = {
+  name: "harshrajisnh",
+  age: 25,
+  department: "IT"
+};
+
+console.log(employee);
+
+console.log("type of :", typeof employee);
+
+const obj2 = new Object();
+
+console.log(typeof obj2);
+
+obj2.name = "Abhijitsinh";
+obj2.age = 28;
+obj2.department = "Developer";
+
+console.log("object detail :", obj2);
