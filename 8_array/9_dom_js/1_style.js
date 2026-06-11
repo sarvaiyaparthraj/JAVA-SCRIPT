5c3f87649e0da24641e82101e054c7bdbe7845e4
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-06-11 13:38:46 +0530

    Add files via upload

---
+++ 8_array/9_dom_js/1_style.js
@@ -0,0 +1,5 @@
+document.getElementById("heading").innerText= 'Mount test app';
+
+document.getElementsByClassName("work")[0].innerHTML= "finalHTMl";
+
+let TagSelector=document.getElementsByTagName("h2")[0].innerHTML="first heading"
