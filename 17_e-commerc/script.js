7e14bbbde50d309a9434eddd6ad02c8291cdb550
Parthrajsinh Sarvaiya <sarvaiyaparthrajsinh1818@gmail.com>
2026-07-24 13:56:40 +0530

    Add files via upload

--- 17_e-commerc/script.js
+++ 17_e-commerc/script.js
@@ -1,165 +1,255 @@
-const carItems = [
-  {
-    name: "BMW M4 Competition",
-    price: 8499000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1612544448445-b8232cff3b6c?q=80&w=1074&auto=format&fit=crop"
-  },
-  {
-    name: "Mercedes-Benz G-Class",
-    price: 17200000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1718128769487-7ecc0ec06f97?q=80&w=687&auto=format&fit=crop"
-  },
-  {
-    name: "Audi RS7",
-    price: 19400000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?q=80&w=1174&auto=format&fit=crop"
-  },
-  {
-    name: "Lamborghini Huracán EVO",
-    price: 40000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1566473965997-3de9c817e938?q=80&w=1170&auto=format&fit=crop"
-  },
-  {
-    name: "Ferrari 488 GTB",
-    price: 39000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1597687210367-a4915552d886?w=500&auto=format&fit=crop&q=60"
-  },
-  {
-    name: "Rolls-Royce Phantom",
-    price: 95000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1696233016084-30c8345d85ff?q=80&w=687&auto=format&fit=crop"
-  },
-  {
-    name: "Porsche 911 Turbo S",
-    price: 34000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1679478878852-bb238b7db287?q=80&w=1170&auto=format&fit=crop"
-  },
-  {
-    name: "Bugatti Chiron",
-    price: 300000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1627454820516-dc767bcb4d3e?q=80&w=687&auto=format&fit=crop"
-  },
-  {
-    name: "McLaren 720S",
-    price: 46500000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1664713907576-1c00104ca1ba?q=80&w=627&auto=format&fit=crop"
-  },
-  {
-    name: "Tesla Model S Plaid",
-    price: 11000000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1698514326382-603bb5b273de?q=80&w=687&auto=format&fit=crop"
-  },
-  {
-    name: "Ford Mustang GT",
-    price: 8500000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1547744152-14d985cb937f?q=80&w=1170&auto=format&fit=crop"
-  },
-  {
-    name: "Toyota Supra GR",
-    price: 9200000,
-    quantity: 1,
-    image: "https://images.unsplash.com/photo-1643947867681-bd2a3e22eb02?q=80&w=764&auto=format&fit=crop"
-  }
-];
 
 
-function showProduct() {
-
-  const product = document.getElementById("product-list");
-
-  product.innerHTML = "";
-
-  carItems.forEach((e) => {
-
-    product.innerHTML += `
-      <div class="col-md-4 mb-4">
-
-        <div class="card h-100 shadow">
-
-          <img src="${e.image}" class="card-img-top" style="height:220px; object-fit:cover;" alt="${e.name}">
-
-          <div class="card-body">
-
-            <h5 class="card-title">${e.name}</h5>
-
-            <p class="card-text fw-bold text-success">
-              ₹${e.price.toLocaleString("en-IN")}
-            </p>
-
-            <p>Quantity : ${e.quantity}</p>
-
-            <button
-class="btn btn-success w-100"
-onclick="addToCart(${carItems.indexOf(e)})">
-Add To Cart
-</button>
+let products = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let editId = null;
 
 
 
-          </div>
+async function getProducts() {
 
-        </div>
+    try {
 
-      </div>
-    `;
+        const response = await fetch("https://fakestoreapi.com/products");
 
-  });
+        products = await response.json();
+
+        showProducts(products);
+
+        updateCartCount();
+
+    }
+
+    catch (error) {
+
+        console.log(error);
+
+    }
 
 }
 
-showProduct();
+getProducts();
+
+function showProducts(data) {
+
+    const productList = document.getElementById("product-list");
+
+    productList.innerHTML = "";
+
+    data.forEach((p) => {
+
+        productList.innerHTML += `
+
+        <div class="col-lg-4 col-md-6 mt-4">
+
+            <div class="card h-100 shadow">
+
+                <img src="${p.image}"
+                class="card-img-top"
+                style="height:250px; object-fit:contain;">
+
+                <div class="card-body text-center">
+
+                    <h5>${p.title}</h5>
+
+                    <h4 class="text-success">
+                        ₹${Math.round(p.price * 85)}
+                    </h4>
+
+                    <button
+                    class="btn btn-success w-100 mt-2"
+                    onclick="addToCart(${p.id})">
+
+                    Add To Cart
+
+                    </button>
+
+                    <button
+                    class="btn btn-warning w-100 mt-2"
+                    onclick="editProduct(${p.id})"
+                    data-bs-toggle="modal"
+                    data-bs-target="#addProductModal">
+
+                    Edit
+
+                    </button>
+
+                    <button
+                    class="btn btn-danger w-100 mt-2"
+                    onclick="deleteProduct(${p.id})">
+
+                    Delete
+
+                    </button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
 
 
 
-const data = {
-  name: "PARTH",
-  work: "Web Developer"
-};
+function searchProduct() {
 
-// Save
-localStorage.setItem("data", JSON.stringify(data));
+    let value = document.getElementById("search").value.toLowerCase();
 
-// Get
-const user = JSON.parse(localStorage.getItem("data"));
+    let filterData = products.filter((item) =>
+        item.title.toLowerCase().includes(value)
+    );
 
-console.log(user);
+    showProducts(filterData);
 
+}
 
 
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-updateCartCount();
 
-function addToCart(index){
 
-    let product = carItems[index];
+const productForm = document.getElementById("productForm");
 
-    let exist = cart.find(item => item.name === product.name);
+productForm.addEventListener("submit", function (e) {
 
-    if(exist){
+    e.preventDefault();
 
-        exist.quantity++;
+    let title = document.getElementById("productName").value;
 
-    }else{
+    let price = document.getElementById("productPrice").value;
+
+    let image = document.getElementById("productImage").value;
+
+    let newProduct = {
+
+        id: Date.now(),
+
+        title: title,
+
+        price: Number(price),
+
+        image: image
+
+    };
+
+    products.unshift(newProduct);
+
+    showProducts(products);
+
+    productForm.reset();
+
+    let modal = bootstrap.Modal.getInstance(document.getElementById("addProductModal"));
+
+    modal.hide();
+
+    alert("Product Added Successfully");
+
+});
+
+
+
+function editProduct(id) {
+
+    editId = id;
+
+    let product = products.find((item) => item.id === id);
+
+    document.getElementById("productName").value = product.title;
+    document.getElementById("productPrice").value = product.price;
+    document.getElementById("productImage").value = product.image;
+
+    document.querySelector(".modal-title").innerHTML = "Edit Product";
+
+    document.querySelector("#productForm button[type='submit']").innerHTML = "Update Product";
+}
+
+
+
+
+
+
+productForm.addEventListener("submit", function (e) {
+
+    if (editId == null) return;
+
+    e.preventDefault();
+
+    let product = products.find((item) => item.id === editId);
+
+    product.title = document.getElementById("productName").value;
+
+    product.price = Number(document.getElementById("productPrice").value);
+
+    product.image = document.getElementById("productImage").value;
+
+    showProducts(products);
+
+    productForm.reset();
+
+    editId = null;
+
+    document.querySelector(".modal-title").innerHTML = "Add Product";
+
+    document.querySelector("#productForm button[type='submit']").innerHTML = "Add Product";
+
+    let modal = bootstrap.Modal.getInstance(document.getElementById("addProductModal"));
+
+    modal.hide();
+
+    alert("Product Updated Successfully");
+
+});
+
+
+
+
+
+function deleteProduct(id) {
+
+    let check = confirm("Are you sure you want to delete this product ?");
+
+    if (check) {
+
+        products = products.filter((item) => item.id !== id);
+
+        showProducts(products);
+
+        alert("Product Deleted Successfully");
+
+    }
+
+}
+
+
+
+
+function addToCart(id) {
+
+    let product = cart.find((item) => item.id === id);
+
+    if (product) {
+
+        product.qty++;
+
+    } else {
+
+        let newProduct = products.find((item) => item.id === id);
 
         cart.push({
-            ...product
+
+            ...newProduct,
+
+            qty: 1
+
         });
 
     }
 
-    localStorage.setItem("cart",JSON.stringify(cart));
+    updateLocalStorage();
 
     updateCartCount();
 
@@ -167,18 +257,218 @@ function addToCart(index){
 
 }
 
-function updateCartCount(){
 
-    document.getElementById("cart-count").innerHTML = cart.length;
+
+
+
+
+function updateLocalStorage() {
+
+    localStorage.setItem("cart", JSON.stringify(cart));
 
 }
-function updateCartCount(){
 
-    let count = document.getElementById("cart-count");
 
-    if(count){
 
-        count.innerHTML = cart.length;
+
+
+
+function updateCartCount() {
+
+    let total = 0;
+
+    cart.forEach((item) => {
+
+        total += item.qty;
+
+    });
+
+    document.getElementById("cartCount").innerHTML = total;
+
+}
+
+
+
+
+
+function showCartModal() {
+
+    let modal = new bootstrap.Modal(document.getElementById("cartModal"));
+
+    modal.show();
+
+    showCartItems();
+
+}
+
+
+
+
+function showCartItems() {
+
+    let cartTable = document.getElementById("cartTable");
+
+    cartTable.innerHTML = "";
+
+    let totalQty = 0;
+
+    let grandTotal = 0;
+
+    cart.forEach((p) => {
+
+        totalQty += p.qty;
+
+        grandTotal += Math.round(p.price * 85) * p.qty;
+
+        cartTable.innerHTML += `
+
+        <tr>
+
+            <td>${p.id}</td>
+
+            <td>
+                <img src="${p.image}" width="60" height="60" style="object-fit:contain;">
+            </td>
+
+            <td>${p.title}</td>
+
+            <td>₹${Math.round(p.price * 85)}</td>
+
+            <td>
+
+                <button
+                class="btn btn-success btn-sm"
+                onclick="increaseQty(${p.id})">
+
+                +
+
+                </button>
+
+                <span class="mx-2">${p.qty}</span>
+
+                <button
+                class="btn btn-danger btn-sm"
+                onclick="decreaseQty(${p.id})">
+
+                -
+
+                </button>
+
+            </td>
+
+            <td>
+
+                ₹${Math.round(p.price * 85) * p.qty}
+
+            </td>
+
+            <td>
+
+                <button
+                class="btn btn-danger btn-sm"
+                onclick="removeItem(${p.id})">
+
+                Remove
+
+                </button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+    document.getElementById("totalQty").innerHTML = totalQty;
+
+    document.getElementById("grandTotal").innerHTML = grandTotal;
+
+}
+
+
+
+
+
+function increaseQty(id) {
+
+    let product = cart.find((item) => item.id === id);
+
+    if (product) {
+
+        product.qty++;
+
+    }
+
+    updateLocalStorage();
+
+    updateCartCount();
+
+    showCartItems();
+
+}
+
+
+
+
+function decreaseQty(id) {
+
+    let product = cart.find((item) => item.id === id);
+
+    if (product) {
+
+        if (product.qty > 1) {
+
+            product.qty--;
+
+        } else {
+
+            cart = cart.filter((item) => item.id !== id);
+
+        }
+
+    }
+
+    updateLocalStorage();
+
+    updateCartCount();
+
+    showCartItems();
+
+}
+
+
+
+
+
+function removeItem(id) {
+
+    cart = cart.filter((item) => item.id !== id);
+
+    updateLocalStorage();
+
+    updateCartCount();
+
+    showCartItems();
+
+}
+
+
+
+
+function clearCart() {
+
+    let check = confirm("Are you sure you want to clear the cart?");
+
+    if (check) {
+
+        cart = [];
+
+        updateLocalStorage();
+
+        updateCartCount();
+
+        showCartItems();
 
     }
 
